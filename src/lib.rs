6284adb4a527c61ccd008391abff1da4444d2e95
Dean Rumsby <dean@deanrumsby.com>
2024-03-24 11:12:56 +0000

    feat: implement BPL instruction

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,7 @@ enum Op {
     BIT(u16),
     BMI(u16),
     BNE(u16),
+    BPL(u16),
 }
 
 enum Flag {
@@ -125,6 +126,11 @@ impl<T: Bus> Cpu<T> {
             },
             Op::BNE(x) => {
                 if !self.p[Flag::Zero] {
+                    self.pc += x;
+                }
+            },
+            Op::BPL(x) => {
+                if !self.p[Flag::Negative] {
                     self.pc += x;
                 }
             },
@@ -805,6 +811,40 @@ mod tests {
         expected = cpu.pc;
         cpu.p[Flag::Zero] = true;
         cpu.execute(Op::BNE(x));
+        result = cpu.pc;
+        assert!(
+            result == expected,
+            "Incorrect PC value on non-branching. Expected {}, Result {}",
+            result,
+            expected
+        );
+    }
+
+    #[test]
+    fn op_bpl() {
+        let mut cpu = Cpu::new(TestBus::new());
+        let (mut x, mut result, mut expected);
+
+        // branches when negative flag clear
+        cpu.reset();
+        x = 0x0532;
+        expected = cpu.pc + x;
+        cpu.p[Flag::Negative] = false;
+        cpu.execute(Op::BPL(x));
+        result = cpu.pc;
+        assert!(
+            result == expected,
+            "Incorrect PC value on branching. Expected {}, Result {}",
+            result,
+            expected
+        );
+
+        // does not branch when negative flag set
+        cpu.reset();
+        x = 0x2222;
+        expected = cpu.pc;
+        cpu.p[Flag::Negative] = true;
+        cpu.execute(Op::BPL(x));
         result = cpu.pc;
         assert!(
             result == expected,
